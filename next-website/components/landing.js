c3e5dbdbd3cb94071866935ca2812ce5a0ccfdb4
colin <satherc@outlook.com>
2022-05-21 00:57:42 -0400

    todo: diagram the changes

--- next-website/components/landing.js
+++ next-website/components/landing.js
@@ -1,30 +1,5 @@
 const handleClick = (e) => {
     console.log("dis boi just clicked, woah")
-    var modal = document.getElementById("myModal")
-    var btn = document.getElementById("myBtn")
-    var span = document.getElementsByClassName("close")[0]
-    
-    if (modal.style.display !== "block")
-        modal.style.display = "block"
-        
-    // When the user clicks the button, open the modal
-    /* 
-    btn.onclick = function() {
-      modal.style.display = "block"
-    }
-  
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none"
-    }
-  
-    // close window on click outside of modal
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none"
-      }
-    }
-    */
 }
 
 function Landing() {
